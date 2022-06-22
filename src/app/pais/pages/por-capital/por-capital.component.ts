4d5e52713471ce0516f005c9b979957425b30ca7
blilllly <alvearbilly@gmail.com>
2022-06-22 13:43:28 -0500

    view

---
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-por-capital',
+  templateUrl: './por-capital.component.html',
+  styles: [
+  ]
+})
+export class PorCapitalComponent {
+
+  termino: string = 'Hola Mundo';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor( private paisService: PaisService) { }
+
+  buscar(termino: string) {
+
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarCapital(this.termino)
+      .subscribe ({
+        next: (paises) => {
+          console.log(paises);
+          this.paises = paises;
+        },
+        error: (err) => {
+          this.hayError= true;
+          this.paises = [];
+        }
+      });
+  }
+
+  sugerencias( termino: string) {
+
+    this.hayError = false;
+    //TODO: crear sugerencias
+
+  }
+
+}
